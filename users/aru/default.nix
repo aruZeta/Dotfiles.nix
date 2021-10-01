1eb2e0e21aa4ce6c3914272f0ea15998aafd7ad3
aru-hackZ <aru_hackZ.official@zohomail.eu>
2021-10-01 12:10:59 +0200

    [add] Xdg config

--- users/aru/default.nix
+++ users/aru/default.nix
@@ -8,5 +8,6 @@
     ./etc/gpg/default.nix
     ./etc/home/default.nix
     ./etc/sway/default.nix
+    ./etc/xdg/default.nix
   ];
 }
---
+++ users/aru/etc/xdg/default.nix
@@ -0,0 +1,21 @@
+{ ...
+}:
+
+{
+  xdg = {
+    enable = true;
+    userDirs = {
+      enable = true;
+      desktop = "\$HOME/Tmp/Desktop";
+      documents = "\$HOME/Archive";
+      download = "\$HOME/Tmp";
+      music = "\$HOME/Archive/Audio";
+      pictures = "\$HOME/Archive/Images";
+      templates = "\$HOME/Archive/Templates";
+      videos = "\$HOME/Archive/Video";
+      extraConfig = {
+        XDG_DOTFILES_DIR = "\$HOME/Dotfiles";
+      };
+    };
+  };
+}
