2a7d0d5613c2da25bcebbce9ccccfc12f53268f7
aru-hackZ <aru_hackZ.official@zohomail.eu>
2021-10-05 21:17:55 +0200

    [add] Adding neovim nightly using an overlay and minimal config
    I should pin the package, I'm tired of recompiling it everyday.

---
+++ users/aru/etc/overlays/default.nix
@@ -0,0 +1,16 @@
+{ ...
+}:
+
+{
+  nixpkgs.overlays = [
+    # Neovim nightly overlay
+    (import (builtins.fetchTarball {
+      url = https://github.com/nix-community/neovim-nightly-overlay/archive/master.tar.gz;
+    }))
+
+    # Emacs overlay
+    (import (builtins.fetchTarball {
+      url = https://github.com/nix-community/emacs-overlay/archive/master.tar.gz;
+    }))
+  ];
+}
