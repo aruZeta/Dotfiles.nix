17e9cb52c92fb66f843e6f2692d0e12839c81db7
aru-hackZ <aru_hackZ.official@zohomail.eu>
2021-10-30 23:17:26 +0200

    [fix, add] Fixing eof and adding imports

--- users/aru/default.nix
+++ users/aru/default.nix
@@ -8,6 +8,9 @@
     ./etc/git/default.nix
     ./etc/gpg/default.nix
     ./etc/home/default.nix
+    ./etc/java/default.nix
+    ./etc/nvim/default.nix
+    ./etc/overlays/default.nix
     ./etc/sway/default.nix
     ./etc/xdg/default.nix
   ];
