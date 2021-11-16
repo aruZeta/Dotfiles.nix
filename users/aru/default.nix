92c511167d09617209261cb925c0b8483a10881a
aru-hackZ <aru_hackZ.official@zohomail.eu>
2021-11-16 19:35:46 +0100

    [remove] Unnecessary 'default.nix' in import paths

--- users/aru/default.nix
+++ users/aru/default.nix
@@ -4,19 +4,19 @@
 {
   # Importing stuff
   imports = [
-    ./etc/alacritty/default.nix
-    ./etc/eclipse/default.nix
-    ./etc/emacs/default.nix
-    ./etc/git/default.nix
-    ./etc/gpg/default.nix
-    ./etc/home/default.nix
-    ./etc/java/default.nix
-    ./etc/nvim/default.nix
-    ./etc/overlays/default.nix
-    ./etc/starship/default.nix
-    ./etc/sway/default.nix
-    ./etc/waybar/default.nix
-    ./etc/xdg/default.nix
-    ./etc/zsh/default.nix
+    ./etc/alacritty
+    ./etc/eclipse
+    ./etc/emacs
+    ./etc/git
+    ./etc/gpg
+    ./etc/home
+    ./etc/java
+    ./etc/nvim
+    ./etc/overlays
+    ./etc/starship
+    ./etc/sway
+    ./etc/waybar
+    ./etc/xdg
+    ./etc/zsh
   ];
 }
