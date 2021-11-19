ef9c4ec005db73f7d9771f586894188dd2c3145a
aru-hackZ <aru_hackZ.official@zohomail.eu>
2021-11-19 13:18:35 +0100

    [remove] Unnecessary '\' in xdg userDirs

--- users/aru/etc/xdg/default.nix
+++ users/aru/etc/xdg/default.nix
@@ -12,16 +12,16 @@
 
     userDirs = {
       enable = true;
-      desktop = "\$HOME/Tmp/Desktop";
-      documents = "\$HOME/Archive";
-      download = "\$HOME/Tmp";
-      music = "\$HOME/Archive/Audio";
-      pictures = "\$HOME/Archive/Images";
-      templates = "\$HOME/Archive/Templates";
-      videos = "\$HOME/Archive/Video";
+      desktop = "$HOME/Tmp/Desktop";
+      documents = "$HOME/Archive";
+      download = "$HOME/Tmp";
+      music = "$HOME/Archive/Audio";
+      pictures = "$HOME/Archive/Images";
+      templates = "$HOME/Archive/Templates";
+      videos = "$HOME/Archive/Video";
 
       extraConfig = {
-        XDG_DOTFILES_DIR = "\$HOME/Dotfiles";
+        XDG_DOTFILES_DIR = "$HOME/Dotfiles";
       };
     };
   };
