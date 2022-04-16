855cc42ea4395e13ae72229fb756a483c1a12a09
aru-hackZ <aru_hackZ.official@zohomail.eu>
2022-04-16 16:19:43 +0200

    [refactor] Move scripts in /bin to user-defined scripts

--- bin/apply-system.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-pushd $(xdg-user-dir DOTFILES)
-sudo nixos-rebuild switch -I nixos-config=./system/configuration.nix
-popd
--- bin/apply-users.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-pushd $(xdg-user-dir DOTFILES)
-home-manager switch -f ./users/$USER/default.nix
-popd
