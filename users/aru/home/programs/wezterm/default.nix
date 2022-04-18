18573a79ae4dfefeeedd2de034f6e2b00a52ee7e
aru-hackZ <aru_hackZ.official@zohomail.eu>
2022-04-18 16:41:37 +0200

    [add] Missed to add the note on wezterm config

--- users/aru/home/programs/wezterm/default.nix
+++ users/aru/home/programs/wezterm/default.nix
@@ -2,6 +2,9 @@
 }:
 
 {
+  # Note:
+  # This option doesn't exist yet, just setting it for future usage when the
+  # module is created and using atm so I can install packages if it is enabled
   programs.wezterm = {
     enable = true;
   };
