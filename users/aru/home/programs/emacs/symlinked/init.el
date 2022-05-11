976377103d52c8b5ae9b50a061a0160dc5bf5ea3
aru-hackZ <aru_hackZ.official@zohomail.eu>
2022-05-11 16:19:47 +0200

    [add] Emacs minimal evil config

--- users/aru/home/programs/emacs/symlinked/init.el
+++ users/aru/home/programs/emacs/symlinked/init.el
@@ -3,3 +3,14 @@
 (setq user-full-name    "Alberto Robles Gomez"
       user-mail-address "aru_hackZ.official@zohomail.eu")
 
+;;;; Evil
+
+(setq evil-want-keybinding nil)
+
+(evil-mode 1)
+(evil-collection-init)
+
+(setq evil-collection-outline-bind-tab-p t
+      evil-collection-calendar-want-org-bindings t
+      evil-collection-setup-minibuffer t)
+
