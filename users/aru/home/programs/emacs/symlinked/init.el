76ebcfdddc9590df634bd5b19a6a7889951b0da4
aru-hackZ <aru_hackZ.official@zohomail.eu>
2022-05-14 11:03:54 +0200

    [add] Emacs macro to generate color themes

--- users/aru/home/programs/emacs/symlinked/init.el
+++ users/aru/home/programs/emacs/symlinked/init.el
@@ -44,6 +44,49 @@
 
 (blink-cursor-mode 0)
 
+;;;; Colors
+
+(defgroup aru/colors nil
+  "Group for color themes."
+  :prefix "aru/colors/")
+
+(defmacro aru/generate-color-theme (theme-name name-color-alist)
+  "Creates variables via defcustom and adds them to group aru/colors/THEME-NAME.
+The name of each variable is aru/colors/THEME-NAME/NAME and the value is COLOR."
+  (let* ((color-group "aru/colors")
+         (theme-full (concat color-group "/" theme-name))
+         (theme-symbol (intern theme-full))
+         (f-list
+          `((defgroup ,theme-symbol nil
+              ,(concat "Colors from the " theme-name " theme.")
+              :prefix ,(concat theme-full "/")
+              :group  'aru/colors))))
+    (dolist (name-color-cons name-color-alist `(progn ,@f-list))
+      (let ((name (car name-color-cons))
+            (color (cdr name-color-cons)))
+        (push `(defcustom ,(intern (concat theme-full "/" name)) ,color
+                 ,(concat name " color of " theme-name " theme.")
+                 :group (quote ,theme-symbol)
+                 :type 'string)
+              f-list)))))
+
+(aru/generate-color-theme "rose-pine-dawn"
+                          (("base"           . "#faf4ed")
+                           ("surface"        . "#fffaf3")
+                           ("overlay"        . "#f2e9e1")
+                           ("muted"          . "#9893a5")
+                           ("subtle"         . "#797593")
+                           ("text"           . "#575279")
+                           ("love"           . "#b4637a")
+                           ("gold"           . "#ea9d34")
+                           ("rose"           . "#d7827e")
+                           ("pine"           . "#286983")
+                           ("foam"           . "#56949f")
+                           ("iris"           . "#907aa9")
+                           ("highlight-low"  . "#f4ede8")
+                           ("highlight-med"  . "#dfdad9")
+                           ("highlight-high" . "#cecacd")))
+
 ;;;; Fonts
 
 (defun set-face (face style)
