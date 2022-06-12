013e1e622478664d5a4118394049ca5fc11b781e
aru-hackZ <aru_hackZ.official@zohomail.eu>
2022-06-12 17:58:33 +0200

    [add] Requiered packages to use eslint lsp in emacs

--- users/aru/home/programs/emacs/packages.nix
+++ users/aru/home/programs/emacs/packages.nix
@@ -8,7 +8,10 @@ with pkgs;
 usefulExpresions.condAndValuesList [
   { cond = enabledStuff.programs.emacs.enable;
     vals = [
+      # Need to figure out how to get this packages from a nix-shell to emacs
       nodePackages.vscode-langservers-extracted
+      nodePackages.eslint
+      nodejs
     ];
   }
 ]
