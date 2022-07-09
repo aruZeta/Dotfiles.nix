a5c3c2012f2927300c2bdc899501220649d475d1
aru-hackZ <aru_hackZ.official@zohomail.eu>
2022-07-09 11:22:08 +0200

    [add] Emacs fix lag switching issue with ivy

--- users/aru/home/programs/emacs/symlinked/init.el
+++ users/aru/home/programs/emacs/symlinked/init.el
@@ -209,6 +209,24 @@
 (ivy-rich-mode 1)
 (all-the-icons-ivy-rich-mode)
 
+;;; Lag switching buffer issue fix from
+;;; https://github.com/Yevgnen/ivy-rich/issues/87#issuecomment-689581896=
+
+(defvar ivy-rich--ivy-switch-buffer-cache
+  (make-hash-table :test 'equal))
+
+(define-advice ivy-rich--ivy-switch-buffer-transformer
+    (:around (old-fn x) cache)
+  (let ((ret (gethash x ivy-rich--ivy-switch-buffer-cache)))
+    (unless ret
+      (setq ret (funcall old-fn x))
+      (puthash x ret ivy-rich--ivy-switch-buffer-cache))
+    ret))
+
+(define-advice +ivy/switch-buffer
+    (:before (&rest _) ivy-rich-reset-cache)
+  (clrhash ivy-rich--ivy-switch-buffer-cache))
+
 ;;;; Counsel
 
 (counsel-mode 1)
