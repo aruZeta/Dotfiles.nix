4c3047939afebc58730e41345e9eec378f74ff7e
aruZeta <me@aruzeta.com>
2023-02-21 18:51:37 +0100

    [fix] Home manager hash

--- default.nix
+++ default.nix
@@ -5,7 +5,7 @@ let
   home-manager = builtins.fetchTarball {
     name = "home-manager-unstable";
     url = "https://github.com/nix-community/home-manager/archive/da72e6fc6b7dc0c3f94edbd310aae7cd95c678b5.tar.gz";
-    sha256 = "sha256:0n8lg7dm6cdnr2111pbfg16lqy3slv9a0cg3dy8d1f8ry40ippwn";
+    sha256 = "sha256:08zc9fqniv0r8cgrw4n2gbdpr8bh0q2vf9i2pcmkrxdd2rnm8zms";
   };
 
   evalHome = import "${toString home-manager}/modules";
