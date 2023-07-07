a0775bc5c2961de69494bb717ddc4221f8f53ee7
aruZeta <me@aruzeta.com>
2023-07-07 18:44:58 +0200

    feat: missing ssh key from my new kairos work gpg key

--- users/aru/home/services/gpg-agent/default.nix
+++ users/aru/home/services/gpg-agent/default.nix
@@ -3,7 +3,10 @@
 }:
 
 {
-  sshKeys = [ "4284CFA5CE7E5F985E6F7B5E242E2C7B35EF94E9" ];
+  sshKeys = [
+    "4284CFA5CE7E5F985E6F7B5E242E2C7B35EF94E9"
+    "9F9BC86D61E29C3F97ACBF528223DB2B62D6741B"
+  ];
   defaultCacheTtl    = 3600 * 8;
   maxCacheTtl        = 3600 * 8;
   defaultCacheTtlSsh = 3600 * 8;
