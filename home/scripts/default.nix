bd50539ea34db1025dc5b487fde22b3357eead16
alberto.robles.gomez <alberto.robles.gomez@kairosds.com>
2023-07-08 12:03:53 +0200

    feat: no need to type the .nix

--- home/scripts/default.nix
+++ home/scripts/default.nix
@@ -3,13 +3,13 @@ pkgs: config:
 with pkgs; {
   apply-system = writeShellScriptBin "apply-system" ''
     pushd ${config.xdg.userDirs.extraConfig.XDG_DOTFILES_DIR}
-    sudo nixos-rebuild switch -I nixos-config=./system/$1
+    sudo nixos-rebuild switch -I nixos-config=./system/$1.nix
     popd
   '';
 
   apply-user = writeShellScriptBin "apply-user" ''
     pushd ${config.xdg.userDirs.extraConfig.XDG_DOTFILES_DIR}
-    home-manager switch -f ./home/$1
+    home-manager switch -f ./home/$1.nix
     popd
   '';
 
