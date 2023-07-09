451a2885f5c200c6f000c11837dd43a0f6c4267f
aruZeta <me@aruzeta.com>
2023-07-09 14:08:48 +0200

    feat: home manager as a module

--- system/flake.nix
+++ system/flake.nix
@@ -2,12 +2,17 @@
   inputs = {
     nixpkgs_23_05.url = "github:NixOS/nixpkgs/nixos-23.05";
     nixpkgs_unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+    home-manager_23_05 = {
+      url = "github:nix-community/home-manager/release-23.05";
+      inputs.nixpkgs.follows = "nixpkgs_23_05";
+    };
   };
 
   outputs =
     { self
     , nixpkgs_23_05
     , nixpkgs_unstable
+    , home-manager_23_05
     , ...
     } @ inputs:
 
@@ -29,6 +34,16 @@
 
             # Don't allow dirty git trees
             { system.configurationRevision = noAllowDirty; }
+
+            # Home manager
+            home-manager_23_05.nixosModules.home-manager
+
+            # Home manager config
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users.aru = import ../home/aru.nix;
+            }
           ];
         };
       };
