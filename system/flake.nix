428c056904ac50949a0b225961bf6218463bfce5
aruZeta <me@aruzeta.com>
2023-07-09 12:01:41 +0200

    feat: moving main NixOS config to a flake

---
+++ system/flake.nix
@@ -0,0 +1,36 @@
+{
+  inputs = {
+    nixpkgs_23_05.url = "github:NixOS/nixpkgs/nixos-23.05";
+    nixpkgs_unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    { self
+    , nixpkgs_23_05
+    , nixpkgs_unstable
+    , ...
+    } @ inputs:
+
+    let
+      noAllowDirty =
+        if self ? rev
+        then self.rev
+        else throw "Refusing to build from a dirty Git tree!";
+    in {
+      nixosConfigurations = {
+        "main-laptop.machine.aruZeta.com" = nixpkgs_23_05.lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [
+            # Hardware
+            ./hardware/msi-bravo-15-b5dd.nix
+
+            # Config
+            ../users/aru/system/default.nix
+
+            # Others
+            ({ ... }: { system.configurationRevision = noAllowDirty; })
+          ];
+        };
+      };
+    };
+}
