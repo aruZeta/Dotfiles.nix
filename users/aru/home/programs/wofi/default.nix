d08173bc4a3c50c409a1df73148212077f70124c
aruZeta <me@aruzeta.com>
2023-10-26 21:41:56 +0200

    feat: better wofi config

--- users/aru/home/programs/wofi/default.nix
+++ users/aru/home/programs/wofi/default.nix
@@ -6,12 +6,15 @@
   settings = {
     width = "40%";
     lines = 8;
+    dynamic_lines = true;
 
     allow_images = true;
     image_size = 24;
 
     prompt = "";
     insensitive = true;
+    parse_search = true;
+    hide_search = false;
 
     term = "wezterm";
   };
